ff5f322909d8c1df3c6bfec9fcde0278eacceadc
Harshit564 <47476857+Harshit564@users.noreply.github.com>
2019-03-04 08:31:39 +0530

    Add files via upload

--- app/src/main/java/com/maskytech/www/ieeepec/MainActivity.java
+++ app/src/main/java/com/maskytech/www/ieeepec/MainActivity.java
@@ -1,6 +1,5 @@
 package com.maskytech.www.ieeepec;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
@@ -9,16 +8,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
-private RecyclerView recyclerView;
-private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +22,13 @@ private RecyclerView.LayoutManager layoutManager;
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mDrawerLayout = findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNav);
         Menu menu = bottomNavigationView.getMenu();
         MenuItem menuItem = menu.getItem(0);
         menuItem.setChecked(true);
@@ -43,13 +38,13 @@ private RecyclerView.LayoutManager layoutManager;
 
                 switch (item.getItemId()) {
                     case R.id.feed:
-                        Intent intent1 = new Intent(MainActivity.this, ActivityOne.class);
-                        startActivity(intent1);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new Frag_feeds()).commit();
                         break;
 
                     case R.id.events:
-                        Intent intent2 = new Intent(MainActivity.this, ActivityTwo.class);
-                        startActivity(intent2);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                                new Frag_events()).commit();
                         break;
                 }
                 return false;
@@ -57,6 +52,7 @@ private RecyclerView.LayoutManager layoutManager;
         });
 
     }
+
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         switch (menuItem.getItemId()) {
@@ -68,6 +64,7 @@ private RecyclerView.LayoutManager layoutManager;
         mDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
+
     @Override
     public void onBackPressed() {
         if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
@@ -87,4 +84,4 @@ private RecyclerView.LayoutManager layoutManager;
     }
 
 
-    }
+}
